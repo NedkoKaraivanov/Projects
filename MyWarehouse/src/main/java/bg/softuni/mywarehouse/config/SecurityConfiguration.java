b8ee134a47fd278bb1ede81fd751e79daefcbb76
Nedko Karaivanov <nedko2016@gmail.com>
2023-05-07 21:46:24 +0300

    Added Logout Service

--- MyWarehouse/src/main/java/bg/softuni/mywarehouse/config/SecurityConfiguration.java
+++ MyWarehouse/src/main/java/bg/softuni/mywarehouse/config/SecurityConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 
 @Configuration
@@ -22,6 +23,7 @@ public class SecurityConfiguration {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
+    private final LogoutHandler logoutHandler;
 
 
     @Bean
@@ -42,8 +44,8 @@ public class SecurityConfiguration {
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .logout()
-                .logoutUrl("/api/users/logout")
-//                .addLogoutHandler(logoutHandler)
+                .logoutUrl("/api/auth/logout")
+                .addLogoutHandler(logoutHandler)
                 .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext());
 
         return httpSecurity.build();
