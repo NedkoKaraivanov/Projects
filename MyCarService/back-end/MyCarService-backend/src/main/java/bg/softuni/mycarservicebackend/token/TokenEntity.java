8059e2e6a0b2286f28b3c20c3d3114e0809a73b2
Nedko Karaivanov <nedko2016@gmail.com>
2023-07-26 23:18:31 +0300

    Implemented JwtService and JwtAuthenticationFilter

---
+++ MyCarService/back-end/MyCarService-backend/src/main/java/bg/softuni/mycarservicebackend/token/TokenEntity.java
@@ -0,0 +1,32 @@
+package bg.softuni.mycarservicebackend.token;
+
+import bg.softuni.mycarservicebackend.domain.entities.BaseEntity;
+import bg.softuni.mycarservicebackend.domain.entities.UserEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tokens")
+public class TokenEntity extends BaseEntity {
+
+    @Column(unique = true)
+    public String token;
+
+    @Enumerated(EnumType.STRING)
+    public TokenType tokenType = TokenType.BEARER;
+
+    public boolean revoked;
+
+    public boolean expired;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    public UserEntity user;
+}
---
+++ MyCarService/back-end/MyCarService-backend/src/main/java/bg/softuni/mycarservicebackend/token/TokenType.java
@@ -0,0 +1,5 @@
+package bg.softuni.mycarservicebackend.token;
+
+public enum TokenType {
+    BEARER
+}
