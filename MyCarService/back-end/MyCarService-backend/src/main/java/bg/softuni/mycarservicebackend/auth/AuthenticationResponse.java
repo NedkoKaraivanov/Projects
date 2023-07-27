7102950f78d00d44636885d096b9b13e49b49af8
Nedko Karaivanov <nedko2016@gmail.com>
2023-07-27 11:18:42 +0300

    Created Authentication Service

---
+++ MyCarService/back-end/MyCarService-backend/src/main/java/bg/softuni/mycarservicebackend/auth/AuthenticationResponse.java
@@ -0,0 +1,25 @@
+package bg.softuni.mycarservicebackend.auth;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationResponse {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+    @JsonProperty("roles")
+    private List<String> roles;
+}
