2c3e60bbd893ce1ab321bb60a16a48379df0016c
Nedko Karaivanov <nedko2016@gmail.com>
2023-09-20 20:21:23 +0300

    Added Integration Tests for Authentication Controller

---
+++ MyCarService/MyCarService-backend/src/test/java/bg/softuni/mycarservicebackend/web/AuthenticationControllerIT.java
@@ -0,0 +1,121 @@
+package bg.softuni.mycarservicebackend.web;
+
+import bg.softuni.mycarservicebackend.auth.AuthenticationRequest;
+import bg.softuni.mycarservicebackend.auth.RegisterRequest;
+import bg.softuni.mycarservicebackend.domain.entities.UserEntity;
+import bg.softuni.mycarservicebackend.repositories.UserRepository;
+import bg.softuni.mycarservicebackend.token.TokenRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles("Test")
+public class AuthenticationControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TokenRepository tokenRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeAll
+    void setUp() {
+        UserEntity testUser = UserEntity.builder()
+                .email("userEmail@test.com")
+                .password(passwordEncoder.encode("123123"))
+                .build();
+        userRepository.save(testUser);
+    }
+
+    @AfterAll
+    void tearDown() {
+        tokenRepository.deleteAll();
+        userRepository.deleteAll();
+    }
+
+    @Test
+    void register_Valid_User_Request_Successful() throws Exception {
+        RegisterRequest requestBody = RegisterRequest.builder()
+                .email("newUser@test.com")
+                .password("123123")
+                .build();
+
+        String jsonRequestBody = objectMapper.writeValueAsString(requestBody);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequestBody))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.roles").value("1"));
+    }
+
+    @Test
+    void register_With_Existing_User_Request_Invalid() throws Exception {
+        RegisterRequest requestBody = RegisterRequest.builder()
+                .email("userEmail@test.com")
+                .password("123123")
+                .build();
+
+        String jsonRequestBody = objectMapper.writeValueAsString(requestBody);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequestBody))
+                .andExpect(MockMvcResultMatchers.status().isConflict());
+    }
+
+    @Test
+    void authenticate_With_Valid_User() throws Exception {
+        AuthenticationRequest authRequest = AuthenticationRequest.builder()
+                .email("userEmail@test.com")
+                .password("123123")
+                .build();
+
+        String jsonRequestBody = objectMapper.writeValueAsString(authRequest);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/authenticate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequestBody))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    void authenticate_With_Wrong_Password_Unauthorized_Status_Returned() throws Exception {
+        AuthenticationRequest authRequest = AuthenticationRequest.builder()
+                .email("userEmail@test.com")
+                .password("wrongPassword")
+                .build();
+
+        String jsonRequestBody = objectMapper.writeValueAsString(authRequest);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/authenticate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequestBody))
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
+    }
+
+}
