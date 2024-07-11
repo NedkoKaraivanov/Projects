a00fff203d94b381dd4827a0cc0773d39a59d20c
Nedko Karaivanov <nedko2016@gmail.com>
2024-07-11 10:42:56 +0300

    Added create notification methods

--- MyCarService/MyCarService-backend/src/main/java/bg/softuni/mycarservicebackend/repositories/UserRepository.java
+++ MyCarService/MyCarService-backend/src/main/java/bg/softuni/mycarservicebackend/repositories/UserRepository.java
@@ -1,13 +1,17 @@
 package bg.softuni.mycarservicebackend.repositories;
 
 import bg.softuni.mycarservicebackend.domain.entities.UserEntity;
+import bg.softuni.mycarservicebackend.domain.entities.UserRoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByEmail(String email);
+
+    List<UserEntity> findAllByRolesEquals(List<UserRoleEntity> roles);
 }
--- MyCarService/MyCarService-backend/src/main/java/bg/softuni/mycarservicebackend/services/NotificationService.java
+++ MyCarService/MyCarService-backend/src/main/java/bg/softuni/mycarservicebackend/services/NotificationService.java
@@ -1,12 +1,44 @@
 package bg.softuni.mycarservicebackend.services;
 
+import bg.softuni.mycarservicebackend.domain.entities.NotificationEntity;
+import bg.softuni.mycarservicebackend.domain.entities.UserEntity;
 import bg.softuni.mycarservicebackend.repositories.NotificationRepository;
+import bg.softuni.mycarservicebackend.util.NotificationMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
 
     private final NotificationRepository notificationRepository;
+
+    private final UserService userService;
+
+    protected NotificationEntity createAdminNotification() {
+        List<UserEntity> adminUsers = this.userService.findAdminUsers();
+
+        return NotificationEntity.builder()
+                .receivers(adminUsers)
+                .isRead(false)
+                .time(LocalDateTime.now())
+                .message(NotificationMessage.NEW_BOOKING_NOTIFICATION_MESSAGE)
+                .build();
+    }
+
+    protected NotificationEntity createUserNotification(List<UserEntity> receivers) {
+        return NotificationEntity.builder()
+                .receivers(receivers)
+                .isRead(false)
+                .time(LocalDateTime.now())
+                .message(NotificationMessage.NEW_BOOKING_STATUS_MESSAGE)
+                .build();
+    }
+
+    protected NotificationEntity saveNotification(NotificationEntity notification) {
+        return this.notificationRepository.save(notification);
+    }
 }
